5740800bcd5cca50ec8c4cd43046cbffd967ca43
rpidanny <abhishekmaharjan1993@gmail.com>
2024-07-11 18:36:04 +0200

    feat: update openai models

--- src/containers/Settings/Settings.tsx
+++ src/containers/Settings/Settings.tsx
@@ -4,11 +4,14 @@ import React, { useEffect, useState } from 'react';
 
 enum OpenAIChatModel {
   GPT_4 = 'gpt-4',
-  GPT_4_0314 = 'gpt-4-0314',
-  GPT_4_32K = 'gpt-4-32k',
-  GPT_4_32K_0314 = 'gpt-4-32k-0314',
+  GPT_4_o = 'gpt-4o',
+  GPT_4_TURBO = 'gpt-4-turbo',
+  GPT_4_0613 = 'gpt-4-0613',
   GPT_3_5_TURBO = 'gpt-3.5-turbo',
   GPT_3_5_TURBO_0301 = 'gpt-3.5-turbo-0301',
+  GPT_3_5_TURBO_0125 = 'gpt-3.5-turbo-0125',
+  GPT_3_5_TURBO_1106 = 'gpt-3.5-turbo-1106',
+  GPT_3_5_TURBO_INSTRUCT = 'gpt-3.5-turbo-instruct',
 }
 
 const Settings = () => {
